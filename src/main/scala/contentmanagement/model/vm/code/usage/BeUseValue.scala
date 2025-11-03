095b47b421e9c38d5803472c565e498a270a8dbe
greubela <andre.greubel@hu-berlin.de>
2025-11-04 00:28:10 +0100

    Support lone if statements and inline comments

--- src/main/scala/contentmanagement/model/vm/code/usage/BeUseValue.scala
+++ src/main/scala/contentmanagement/model/vm/code/usage/BeUseValue.scala
@@ -3,27 +3,22 @@ package contentmanagement.model.vm.code.usage
 import contentmanagement.model.vm.code.defining.*
 import contentmanagement.model.vm.types.*
 import contentmanagement.model.vm.code.BeExpression
-import contentmanagement.datastructures.tree.nodeImpl.NodeBasedTreeImpl
-import contentmanagement.model.language.AppLanguage
 import contentmanagement.model.language.{HumanLanguage, ProgrammingLanguage}
 import contentmanagement.model.vm.code.tree.BeExpressionNode
 import contentmanagement.model.vm.simulation.{BeExpressionExecutor, BeSimulatorConfig, BeSimulatorState}
-import contentmanagement.model.vm.types.*
 import interactionPlugins.blockEnvironment.config.BeDisplayConfig
 import interactionPlugins.blockEnvironment.programming.BeExpressionTree
 import interactionPlugins.blockEnvironment.programming.blocks.BeBlock
 import interactionPlugins.blockEnvironment.programming.blocks.variable.BeBlockUseValue
 
 case class BeUseValue(value: BeDataValue, contextIfKnown: Option[BeDefineVariable]) extends BeExpression {
-  
+
   def getInLanguage(programmingLanguage: ProgrammingLanguage, humanLanguage: HumanLanguage): String = value match {
-    case BeDataValueLiteral(literalStr) => {
-      if(contextIfKnown.nonEmpty) contextIfKnown.get.variableType.formatValueForDisplay(literalStr).getInLanguage(programmingLanguage)
-      else literalStr    
-    }
-    case _ => {
-      value.displayAsString
-    }
+    case BeDataValueLiteral(literalStr) if contextIfKnown.nonEmpty =>
+      contextIfKnown.get.variableType.formatValueForDisplay(literalStr).getInLanguage(programmingLanguage)
+    case BeDataValueLiteral(literalStr) => literalStr
+    case reference: BeUseValueReference => reference.variable.name.getInLanguage(humanLanguage)
+    case _ => value.displayAsString
   }
 
   def hasThisExpressionSideEffects: Boolean = false
