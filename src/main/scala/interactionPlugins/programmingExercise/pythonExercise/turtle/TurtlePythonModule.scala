f93bcbe8951c3e99d25614c517d686889ae6b3de
greubela <andre.greubel@hu-berlin.de>
2026-03-31 19:00:06 +0300

    Bind backend into turtle lambdas to avoid NameError

--- src/main/scala/interactionPlugins/programmingExercise/pythonExercise/turtle/TurtlePythonModule.scala
+++ src/main/scala/interactionPlugins/programmingExercise/pythonExercise/turtle/TurtlePythonModule.scala
@@ -9,14 +9,14 @@ object TurtlePythonModule {
       |
       |class _Screen:
       |    def __getattr__(self, name):
-      |        return lambda *args: backend.call_screen(name, *args)
+      |        return lambda *args, _backend=backend: _backend.call_screen(name, *args)
       |
       |class Turtle:
       |    def __init__(self):
       |        self._id = backend.create_turtle()
       |
       |    def __getattr__(self, name):
-      |        return lambda *args: backend.call_turtle(self._id, name, *args)
+      |        return lambda *args, _backend=backend, _id=self._id: _backend.call_turtle(_id, name, *args)
       |
       |_screen = _Screen()
       |_default_id = backend.default_turtle_id()
@@ -25,12 +25,26 @@ object TurtlePythonModule {
       |    return _screen
       |
       |def __getattr__(name):
-      |    return lambda *args: backend.call_turtle(_default_id, name, *args)
+      |    return lambda *args, _backend=backend, _id=_default_id: _backend.call_turtle(_id, name, *args)
       |
       |m = types.ModuleType("turtle")
       |m.Turtle = Turtle
       |m.Screen = Screen
       |m.__getattr__ = __getattr__
+      |m.__all__ = [
+      |    "Turtle", "Screen",
+      |    "forward", "fd",
+      |    "backward", "back", "bk",
+      |    "left", "lt", "right", "rt",
+      |    "goto", "setpos", "setposition", "setx", "sety",
+      |    "setheading", "seth", "home",
+      |    "penup", "pu", "up", "pendown", "pd", "down",
+      |    "pensize", "width",
+      |    "pencolor", "fillcolor", "color",
+      |    "position", "pos", "xcor", "ycor", "heading", "distance",
+      |    "isdown", "showturtle", "st", "hideturtle", "ht", "isvisible",
+      |    "clear", "reset"
+      |]
       |sys.modules["turtle"] = m
       |""".stripMargin
 }
