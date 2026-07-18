04b0aac8c3974de104f83e258497d6a57c8571d8
Andre (Tower) <andre.greubel@hu-berlin.de>
2026-07-18 10:54:22 +0200

    get all named elements

--- modules/core/shared/src/main/scala/it/evadid/vm/parsing/abstractions/GenericAST.scala
+++ modules/core/shared/src/main/scala/it/evadid/vm/parsing/abstractions/GenericAST.scala
@@ -1,16 +1,17 @@
 package it.evadid.vm.parsing.abstractions
 
-import it.evadid.vm.parsing.abstractions.GenericAST.GenericASTListener
+import it.evadid.vm.parsing.abstractions.GenericAST.NamedElement
 
 trait GenericAST {
 
 
+  def allNamedElements(): Map[String, NamedElement] =
+    traversePreOrderWithListener {
+      case (ne: NamedElement) => Some(ne.name, ne.asInstanceOf[NamedElement])
+    }.flatten.toMap
 
-
-
-  def traversePreOrderWithListener(listener: GenericASTListener): Unit = {
-    listener.onNodeVisited(this)
-    getChildren().foreach(_.traversePreOrderWithListener(listener))
+  def traversePreOrderWithListener[T](onNodeVisited: GenericAST => T): Set[T] = {
+    Set[T](onNodeVisited(this)) ++ getChildren().flatMap(_.traversePreOrderWithListener(onNodeVisited))
   }
 
   def getChildren(): Seq[GenericAST] = Seq()
