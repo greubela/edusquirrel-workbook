f2d86d6acc5b9581c56d5dc2584f67ac5e12e190
greubela <andre.greubel@hu-berlin.de>
2026-07-19 15:05:52 +0200

    Split Snap editor and preview rendering

--- modules/client/src/main/scala/it/evadid/homepage/webElements/editor/code/SnapRenderer/BeProgramSnapCustomRenderer.scala
+++ modules/client/src/main/scala/it/evadid/homepage/webElements/editor/code/SnapRenderer/BeProgramSnapCustomRenderer.scala
@@ -1,64 +1,17 @@
 package it.evadid.homepage.webElements.editor.code.SnapRenderer
 
 import com.raquo.airstream.ownership.Owner
-import it.evadid.core.datastructures.language.AppLanguage.{English, Python}
 import it.evadid.vm.BeProgram
-import it.evadid.vm.code.BeExpression
-import it.evadid.vm.code.controlStructures.BeSequence
-import it.evadid.vm.code.others.BeStartProgram
 import org.scalajs.dom.html.Canvas
 
-class BeProgramSnapCustomRenderer extends BeProgramSnapRenderer {
+class BeProgramSnapCustomRenderer extends BeProgramSnapRenderer:
 
-  private val BlockX = 20.0
-  private val FirstBlockY = 20.0
-  private val BlockGap = 8.0
+  override def renderEditorInto(initProgram: BeProgram, canvas: Canvas, config: SnapCodeEditorConfig): Unit =
+    println("BeProgramSnapCustomRenderer.renderEditorInto is not implemented yet")
 
-  override def renderInto(program: BeProgram, canvas: Canvas, config: SnapCodeEditorConfig): Unit =
+  override def renderPreviewInto(program: BeProgram, canvas: Canvas, config: SnapCodeEditorConfig): Unit =
+    println("BeProgramSnapCustomRenderer.renderPreviewInto is not implemented yet")
 
-    val world: WorldMorph = WorldMorph(canvas)
-    val scripts: ScriptsMorph = ScriptsMorph()
+  override def mount(ctx: Owner): Unit = ()
 
-    world.add(scripts)
-    scripts.setPosition(new SnapPoint(0, 0))
-
-    val blocks = blockMorphsFor(program)
-    var y = FirstBlockY
-    blocks.foreach { block =>
-      block.setPosition(new SnapPoint(BlockX, y))
-      scripts.add(block)
-      block.fixLayout()
-      y = y + math.max(block.fullBounds().height(), block.height()) + BlockGap
-    }
-
-    scripts.fixLayout()
-    scripts.changed()
-    world.changed()
-    world.doOneCycle()
-    world
-
-  private def blockMorphsFor(program: BeProgram): List[BlockMorph] =
-    expressionsFor(program.fullProgram).map(blockForExpression)
-
-  private def expressionsFor(expression: BeExpression): List[BeExpression] = expression match
-    case BeStartProgram(Some(sequence)) => expressionsFor(sequence)
-    case BeStartProgram(None) => Nil
-    case sequence: BeSequence if sequence.body.nonEmpty => sequence.body
-    case sequence: BeSequence => List(sequence)
-    case other => List(other)
-
-  private def blockForExpression(expression: BeExpression): BlockMorph =
-    val block = new CommandBlockMorph()
-    block.category = "other"
-    block.setSpec(blockSpecFor(expression))
-    block
-
-  private def blockSpecFor(expression: BeExpression): String =
-    val source = expression.expressionIO.toStringInLanguage(Python, English, skipUnparsable = true).trim
-    if source.nonEmpty then source.replace("%", "%%") else expression.getClass.getSimpleName
-
-
-  override def mount(ctx: Owner): Unit = {}
-
-  override def destroy(): Unit = {}
-}
+  override def destroy(): Unit = ()
